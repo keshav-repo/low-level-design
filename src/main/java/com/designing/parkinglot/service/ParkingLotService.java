759db79c3671f7efe81f2e62fe7e6b0e57cba4b1
keshav <keshav.k987@gmail.com>
2023-03-30 20:37:35 +0530

    adding link for question, input and output file

--- src/main/java/com/designing/parkinglot/service/ParkingLotService.java
+++ src/main/java/com/designing/parkinglot/service/ParkingLotService.java
@@ -4,6 +4,9 @@ import com.designing.parkinglot.model.*;
 
 import java.util.List;
 
+/*
+  https://workat.tech/machine-coding/practice/design-parking-lot-qm6hwq4wkhp8
+ */
 public interface ParkingLotService {
     public ParkingLot addParkingLot(String parkingSlotId, int floorCount, int slotCountPerFloor);
     public List<Slot> getAllSlots(VehicleType vehicleType);
