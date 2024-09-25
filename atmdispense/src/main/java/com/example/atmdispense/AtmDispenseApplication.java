63902fa174c0aeb42a7b4a672069641d2a85edba
Keshav Kumar <keshav.k987@gmail.com>
2024-09-25 12:48:44 +0530

    AtmDispenseApplication rrefactor

--- atmdispense/src/main/java/com/example/atmdispense/AtmDispenseApplication.java
+++ atmdispense/src/main/java/com/example/atmdispense/AtmDispenseApplication.java
@@ -1,19 +1,7 @@
 package com.example.atmdispense;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AtmDispenseApplication implements CommandLineRunner {
-
+public class AtmDispenseApplication{
 	public static void main(String[] args) {
-		SpringApplication.run(AtmDispenseApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
 		DispenseChain dispense50Dollar = new Dollar50Dispenser();
 		DispenseChain dispense20Dollar = new Dollar20Dispenser();
 		DispenseChain dispense10Dollar = new Dollar10Dispenser();
